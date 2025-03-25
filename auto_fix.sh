3f7f31dd67665b28127d97f3fe8bdb5808fe7bad
Dennis Bölling <derdennis1012@gmail.com>
2025-03-25 13:07:06 +0100

    Added SSL Port

--- auto_fix.sh
+++ auto_fix.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-
-docker cp sambaConfig.json nextcloud-fpm:/
-docker exec --user www-data -it nextcloud-fpm /var/www/html/occ files_external:import /sambaConfig.json
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ db:add-missing-indices
