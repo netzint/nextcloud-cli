c99b39419d3b357e7007010e0c3f98c7a74202ef
Dennis Bölling <derdennis1012@gmail.com>
2025-03-26 08:54:38 +0100

    Update auto_fix.sh

--- auto_fix.sh
+++ auto_fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker exec --user www-data -it nextcloud-fpm /var/www/html/occ db:add-missing-indices
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ maintenance:repair
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ maintenance:mimetype:update-db
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ maintenance:mimetype:update-js
