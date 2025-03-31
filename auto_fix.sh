34acb29dc1f38aceeb5493a190d8c8c6e5039e34
Dennis Bölling <derdennis1012@gmail.com>
2025-03-31 11:04:15 +0200

    Updated Folders

--- auto_fix.sh
+++ auto_fix.sh
@@ -14,6 +14,10 @@ docker exec --user www-data -it nextcloud-fpm /var/www/html/occ config:system:se
 docker exec --user www-data -it nextcloud-fpm /var/www/html/occ config:system:set maintenance_window_start --value=2 --type=integer
 docker exec --user www-data -it nextcloud-fpm /var/www/html/occ config:system:set default_phone_region --value="DE" --type=string
 docker exec --user www-data -it nextcloud-fpm bash -c 'echo "# Nextcloud data directory" > /var/www/html/data/.ncdata'
+#enable login protection (bruteforce protection) max 3 tries in 15 minutes
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ config:system:set login_protection_enabled --value=true
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ config:system:set login_protection_delay --value=900
+docker exec --user www-data -it nextcloud-fpm /var/www/html/occ config:system:set login_protection_detection_backend --value=1
 
 
 echo "✅ Nextcloud Konfiguration abgeschlossen."
